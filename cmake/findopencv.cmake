a75be2f7644eb4bad22b41d7f0be921229c805ca
wangyao <1822364545@qq.com>
2024-05-23 11:16:51 +0800

    cmake

--- cmake/findopencv.cmake
+++ cmake/findopencv.cmake
@@ -1,4 +1,6 @@
 find_package(OpenCV REQUIRED)
-add_library(OpenCV::OpenCV INTERFACE)
-target_include_directories(OpenCV::OpenCV INTERFACE ${OpenCV_INCLUDE_DIRS})
-target_link_libraries(OpenCV::OpenCV INTERFACE ${OpenCV_LIBS})
+add_library(OpenCVInterFace INTERFACE)
+target_include_directories(OpenCVInterFace INTERFACE ${OpenCV_INCLUDE_DIRS})
+target_link_libraries(OpenCVInterFace INTERFACE ${OpenCV_LIBS})
+
+add_library(OpenCV::OpenCV ALIAS OpenCVInterFace)
