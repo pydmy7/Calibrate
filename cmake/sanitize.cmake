9ff76a00e7470cd48ceada88b2fd2d879fa942d0
wangyao <1822364545@qq.com>
2024-05-24 00:42:52 +0800

    sanitize

---
+++ cmake/sanitize.cmake
@@ -0,0 +1,38 @@
+macro(useSanitize target_name)
+    isMSVCBackend(msvcbackend)
+    isMinGWBackend(mingwbackend)
+    isGNUBackend(gnubackend)
+    if(${gnubackend} OR ${mingwbackend})
+        target_compile_options(${target} PRIVATE -Wall -Wextra -Wpedantic)
+    elseif(${msvcbackend})
+        target_compile_options(${target} PRIVATE /W4)
+    endif()
+
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        if(${gnubackend})
+            target_compile_options(
+                ${target_name}
+                PRIVATE
+                -fsanitize=address
+                # -fsanitize=thread
+                # -fsanitize=memory
+                # -fsanitize=undefined
+            )
+            target_link_options(
+                ${target_name}
+                PRIVATE
+                -fsanitize=address
+                # -fsanitize=thread
+                # -fsanitize=memory
+                # -fsanitize=undefined
+            )
+        endif()
+
+        if(${mingwbackend})
+        endif()
+
+        if(${msvcbackend})
+        endif()
+    endif()
+    
+endmacro(sanitize)
