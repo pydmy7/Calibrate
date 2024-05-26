2b6b1e777fb27e0d9dac58c0ad1645d84ff39978
wangyao <1822364545@qq.com>
2024-05-26 23:25:04 +0800

    filesystem for output

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,4 +1,5 @@
 #include "chessboard/chessboard.hpp"
+#include "chessboard/cornersort.hpp"
 
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgproc.hpp>
@@ -6,11 +7,15 @@
 
 #include <spdlog/spdlog.h>
 
-#include <iostream>
 #include <unordered_map>
 #include <filesystem>
+#include <string>
+#include <initializer_list>
+#include <utility>
 
-std::unordered_map<int, cornersort::CameraPosition> cameraposition = {
+namespace fs = std::filesystem;
+
+const std::unordered_map<int, cornersort::CameraPosition> cameraposition = {
     {0, cornersort::CameraPosition::front},
     {1, cornersort::CameraPosition::frontright},
     {2, cornersort::CameraPosition::rearright},
@@ -19,80 +24,42 @@ std::unordered_map<int, cornersort::CameraPosition> cameraposition = {
     {5, cornersort::CameraPosition::frontleft}
 };
 
-constexpr std::string_view sourcedir = SOURCE_DIR;
+const fs::path sourceroot {SOURCE_DIR};
+const fs::path inputroot {sourceroot / "images"};
+const fs::path outputroot {sourceroot / "output"};
 
-void testWeiChai() {
-    for (int group = 5; group >= 0; --group) {
-        for (int i = 0; i < 6; ++i) {
-            std::string inputfilepath = fmt::format("{}/images/3-3/weichai/{}/src{}.png", sourcedir, group, i);
-            cv::Mat image = cv::imread(inputfilepath);
-            if (image.empty()) {
-                std::cerr << "image empty\n";
+void testAllImage() {
+    auto solve = [](std::string&& imagepath, std::string&& outputpath) -> void {
+        cv::Mat image = cv::imread(imagepath);
+        std::initializer_list<std::pair<int, int>> patternsize =
+            imagepath.find("3-3") != std::string::npos
+            ? std::initializer_list<std::pair<int, int>>{
+                std::pair<int, int>{3, 3},
+                std::pair<int, int>{3, 3}
+            }
+            : std::initializer_list<std::pair<int, int>>{
+                std::pair<int, int>{2, 2},
+                std::pair<int, int>{2, 2},
+                std::pair<int, int>{3, 4}
+            };
+        cornersort::CameraPosition campos = cameraposition.at(0);
+
+        if (patternsize.size() == 2) {  // 3-3
+            auto pos = imagepath.find("src");
+            if (pos == std::string::npos) {
+                assert(false);
                 return;
             }
-
-            Chessboard cur{image, {{3, 3}, {3, 3}}, cameraposition[i]};
-            std::vector<std::vector<cv::Point>> chessboards = cur.getChessboards();
-            [[maybe_unused]] bool found = !chessboards.empty();
-
-            for (int idx = 0; auto&& corners : chessboards) {
-                for (int j = 0; j < static_cast<int>(corners.size()); ++j) {
-                    cv::putText(image, std::to_string(idx++), corners[j], cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255));
-                    // std::cout << corners[j] << " \n"[j == corners.size() - 1];
-                }
+            auto lpos {pos + 3}, rpos {lpos + 1};
+            while (rpos < imagepath.size() && std::isdigit(imagepath[rpos])) {
+                ++rpos;
             }
-
-            std::string outputdirpath = fmt::format("{}/output/3-3/weichai/{}", sourcedir, group);
-            std::string outputfilepath = fmt::format("{}/src{}.png", outputdirpath, i);
-            std::filesystem::create_directories(outputdirpath);
-            cv::imwrite(outputfilepath, image);
-            std::clog << outputfilepath << '\n';
-        }
-    }
-}
-
-void testJieFang() {
-    for (int group = 4; group >= 0; --group) {
-        for (int i = 0; i < 6; ++i) {
-            std::string inputfilepath = fmt::format("{}/images/3-3/jiefang/{}/src{}.png", sourcedir, group, i);
-            cv::Mat image = cv::imread(inputfilepath);
-            if (image.empty()) {
-                std::cerr << "image empty\n";
-                return;
-            }
-
-            Chessboard cur{image, {{3, 3}, {3, 3}}, cameraposition[i]};
-            std::vector<std::vector<cv::Point>> chessboards = cur.getChessboards();
-            [[maybe_unused]] bool found = !chessboards.empty();
-
-            for (int idx = 0; auto&& corners : chessboards) {
-                for (int j = 0; j < static_cast<int>(corners.size()); ++j) {
-                    cv::putText(image, std::to_string(idx++), corners[j], cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255));
-                    // std::cout << corners[j] << " \n"[j == corners.size() - 1];
-                }
-            }
-
-            std::string outputdirpath = fmt::format("{}/output/3-3/jiefang/{}", sourcedir, group);
-            std::string outputfilepath = fmt::format("{}/src{}.png", outputdirpath, i);
-            std::filesystem::create_directories(outputdirpath);
-            cv::imwrite(outputfilepath, image);
-            std::clog << outputfilepath << '\n';
-        }
-    }
-}
-
-void test34() {
-    for (int i = 31; i >= 0; --i) {
-        std::string inputfilepath = fmt::format("{}/images/3-4/src{}.png", sourcedir, i);
-        cv::Mat image = cv::imread(inputfilepath);
-        if (image.empty()) {
-            std::cerr << "image empty\n";
-            return;
+            int idx {std::stoi(imagepath.substr(lpos, rpos - lpos))};
+            campos = cameraposition.at(idx);
         }
 
-        Chessboard cur{image, {{2, 2}, {2, 2}, {3, 4}}, cameraposition[i]};
+        Chessboard cur(image, patternsize, campos);
         std::vector<std::vector<cv::Point>> chessboards = cur.getChessboards();
-        [[maybe_unused]] bool found = !chessboards.empty();
 
         for (int idx = 0; auto&& corners : chessboards) {
             for (int j = 0; j < static_cast<int>(corners.size()); ++j) {
@@ -101,26 +68,21 @@ void test34() {
             }
         }
 
-        std::string outputdirpath = fmt::format("{}/output/3-4", sourcedir);
-        std::string outputfilepath = fmt::format("{}/src{}.png", outputdirpath, i);
-        std::filesystem::create_directories(outputdirpath);
-        cv::imwrite(outputfilepath, image);
-        std::clog << outputfilepath << '\n';
+        cv::imwrite(outputpath, image);
+    };
+
+    for (auto&& entry  : fs::recursive_directory_iterator(inputroot)) {
+        if (!fs::is_regular_file(entry))  {
+            continue;
+        }
+        fs::create_directories(outputroot / fs::relative(entry.path(), inputroot).parent_path());
+        solve(entry.path().string(), (outputroot / fs::relative(entry.path(), inputroot)).string());
     }
 }
 
 int main() {
 
-    testWeiChai();
-    testJieFang();
-    test34();
-
-    spdlog::info("Hello, {}!", "world");
-    spdlog::warn("This is a warning!");
-    spdlog::error("This is an error!");
-    spdlog::critical("Critical error with number: {}", 42);
-    spdlog::set_pattern("%Y-%m-%d %H:%M:%S.%e %l %n %v");
-    spdlog::info("Custom pattern log example");
+    testAllImage();
 
     return 0;
 }
