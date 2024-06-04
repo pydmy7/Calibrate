68753fa8e5fdb453184b97ad49ebed9fc3901b96
wangyao <1822364545@qq.com>
2024-06-04 20:00:31 +0800

    ci/cd

--- make.sh
+++ make.sh
@@ -12,5 +12,5 @@ cmake -S . -B build -G Ninja
 echo -e "\nbuild: cmake --build build -j"
 cmake --build build -j
 
-echo -e "\nrun: ./build/unittest/unittest.exe"
-./build/unittest/unittest.exe
+echo -e "\nrun: ./build/unittest/unittest"
+./build/unittest/unittest
