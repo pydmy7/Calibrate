dc38d7444bcf085412d3315ae86a667a45326a50
wangyao <1822364545@qq.com>
2024-06-28 19:04:19 +0800

    gnu warning

--- cmake/sanitize.cmake
+++ cmake/sanitize.cmake
@@ -3,7 +3,17 @@ function(useSanitize target_name)
     isMinGWBackend(mingwbackend)
     isGNUBackend(gnubackend)
     if(${gnubackend} OR ${mingwbackend})
-        target_compile_options(${target_name} PRIVATE -Wall -Wextra -Wpedantic)
+        target_compile_options(${target_name} PRIVATE
+            -Wall -Wextra -Weffc++ -Wpedantic
+            -Werror=uninitialized
+            -Werror=return-type
+            -Werror=unused-result
+            -Werror=suggest-override
+            -Wzero-as-null-pointer-constant
+            -Wmissing-declarations
+            -Wold-style-cast
+            -Wnon-virtual-dtor
+        )
     elseif(${msvcbackend})
         target_compile_options(${target_name} PRIVATE /W4)
     endif()
